7fbe4508f2ec38def3a40d298c11760a63283221
TzuYuan <tzuyuanliu0830@gmail.com>
2017-12-20 11:08:37 +0800

    fix styles

--- pages/landingPage.js
+++ pages/landingPage.js
@@ -67,11 +67,11 @@ export default class LandingPage extends Component {
       hintStyle: { color: 'gray' },
       floatingLabelStyle: { color: '#e488ef' },
       floatingLabelFocusStyle: { color: '#e488ef' },
-      underlineStyle: {color: '#fff'},
+      underlineStyle: { color: '#fff' },
       underlineFocusStyle: { borderColor: '#e488ef' },
       inputStyle: { color: '#fff' },
       textareaStyle: { color: '#fff' },
-      style: {marginTop: '10px'}
+      style: { marginTop: '10px' }
     }
     return (
       <FormWrapper>
@@ -154,12 +154,18 @@ export default class LandingPage extends Component {
       }
     }
     let style = {
-      color: '#CCCCCC',
-      fontSize: '19px',
-      display: 'flex',
-      textDecoration: 'none',
-      alignItems: 'center',
-      margin: '26px 0',
+      link: {
+        color: '#CCCCCC',
+        fontSize: '19px',
+        textDecoration: 'none',
+        alignItems: 'center',
+        margin: '26px 0',
+        display: 'flex'
+      },
+      emailIcon: {
+        width: '50px', height: '50px', color: '#fff', marginRight: '10px'
+      },
+      facebookIcon: { marginRight: '10px' },
     }
 
     return (
@@ -169,22 +175,22 @@ export default class LandingPage extends Component {
             <Container>
               <Title>Wizard Amigos CodeCamp 2018</Title>
               <Wrapper>
-              <ButtonWrapper>
-                <Link to="test2" spy={true} smooth={true} offset={50} duration={1000}>
-                <ButtonWhite>LEARN MORE</ButtonWhite>
-                </Link>
-                <Link to="test1" spy={true} smooth={true} offset={50} duration={1500}>
-                <ButtonBlack>
-                    SIGN UP NOW!
-                </ButtonBlack>
-                </Link>
-              </ButtonWrapper>
+                <ButtonWrapper>
+                  <Link to="test2" spy={true} smooth={true} offset={50} duration={1000}>
+                    <ButtonWhite>LEARN MORE</ButtonWhite>
+                  </Link>
+                  <Link to="test1" spy={true} smooth={true} offset={50} duration={1500}>
+                    <ButtonBlack>
+                      SIGN UP NOW!
+                    </ButtonBlack>
+                  </Link>
+                </ButtonWrapper>
               </Wrapper>
             </Container>
           </Scene1>
           <Scene2 name="test2">
             <Container>
-                <SubTitle>Who we are</SubTitle>
+              <SubTitle>Who we are</SubTitle>
               <Wrapper>
                 <ListWrapper>
                   {content.whoWeAre.chinese}
@@ -274,7 +280,7 @@ export default class LandingPage extends Component {
                   3rd Fl., No. 94, Sec. 1, Ba-de Rd., Zhongzheng District, Taipei City 100, Taiwan<br />
 
                   國立臺北科技大學 第六教學大樓B1 B216 (五六日)<br />
-                  B216  6th Academic Building, National Taipei University of Technology (Fri, Sat, Sun)<br />
+                  B216 6th Academic Building, National Taipei University of Technology (Fri, Sat, Sun)<br />
                   106 台北市大安區忠孝東路三段1號<br />
                   1, Sec. 3, Zhongxiao E. Rd., Daan District, Taipei City 106, Taiwan<br />
 
@@ -308,12 +314,12 @@ export default class LandingPage extends Component {
               <SubTitle>Sign Up</SubTitle>
               {isSendingFormSuccess ?
                 <SuccessSentWrapper>
-                  <SubTitle style={{color: '#e488ef'}}>太棒了！</SubTitle>
+                  <SubTitle style={{ color: '#e488ef' }}>太棒了！</SubTitle>
                   <SuccessSentInnerWrapper>
-                  報名表單已成功送出，請確認您的email {email}
-                  我們將會以此mail通知您後續課程訊息 ：）
-                    <br/>
-                    <br/>
+                    報名表單已成功送出，請確認您的email {email}
+                    我們將會以此mail通知您後續課程訊息 ：）
+                    <br />
+                    <br />
                     期待在課程上見到您！
                   </SuccessSentInnerWrapper>
                 </SuccessSentWrapper> :
@@ -326,10 +332,15 @@ export default class LandingPage extends Component {
           </Scene6>
           <Footer>
             <Wrapper>
-              <a style={style} href="mailto:wizardamigos.codecamp@gmail.com"><EmailIcon style={{width: '50px', height: '50px', color: '#fff', marginRight: '10px'}} />wizardamigos.codecamp@gmail.com</a>
-              <a style={style} target="_blank" href="https://www.facebook.com/groups/369246343421803/"><FaceBookIcon style={{marginRight: '10px'}}/>WizardAmigos Codecamp on Facebook</a>
-              WizardAmigos is a FREE open source, open collaborative, and free DIY programming school from Berlin, now
-              starting in Taipei. People of all skill levels and backgrounds are welcome to join and learn together.
+              <Contact><a style={style.link} href="mailto:wizardamigos.codecamp@gmail.com"><EmailIcon
+                style={ style.emailIcon } />wizardamigos.codecamp@gmail.com</a></Contact>
+              <Contact><a style={style.link} target="_blank"
+                          href="https://www.facebook.com/groups/369246343421803/"><FaceBookIcon
+                style={ style.facebookIcon } />WizardAmigos Codecamp on Facebook</a></Contact>
+              <FooterIntro> WizardAmigos is a FREE open source, open collaborative, and free DIY programming school from
+                Berlin, now
+                starting in Taipei. People of all skill levels and backgrounds are welcome to join and learn
+                together.</FooterIntro>
             </Wrapper>
           </Footer>
         </div>
@@ -339,260 +350,265 @@ export default class LandingPage extends Component {
 }
 
 const Black = '#030707'
-
+const Contact = styled.div`
+display: flex;
+align-items: center;
+`
+const FooterIntro = styled.div`
+margin-top: 30px;
+`
 const Container = styled.div`
-width: 100%;
-color: #fff;
-display: flex;
-flex-direction: column;
-`
+  width: 100%;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  `
 const Wrapper = styled.div`
-width: 80%;
-margin: auto;
-padding: 40px 0;
-`
+  width: 80%;
+  margin: auto;
+  padding: 40px 0;
+  `
 const Scene1 = styled.div`
-height: 800px;
-background-image: url('/static/main_background1.jpg');
-background-size:cover;
-background-position: center;
-`
+  height: 800px;
+  background-image: url('/static/main_background1.jpg');
+  background-size:cover;
+  background-position: center;
+  `
 const Title = styled.div`
-display: flex;
-font-size: 60px;
-justify-content: center;
-margin: 395px auto 25px;
-@media(max-width: 768px) {
-font-size: 6vw;
-}
-`
+  display: flex;
+  font-size: 60px;
+  justify-content: center;
+  margin: 395px auto 25px;
+  @media(max-width: 768px) {
+    font-size: 6vw;
+  }
+  `
 const ButtonWrapper = styled.div`
-display: flex;
-justify-content: center;
-`
+  display: flex;
+  justify-content: center;
+  `
 const Button = styled.div`
-font-size: 30px;
-padding: 10px;
-cursor: pointer;
-text-align: center;
-`
+  font-size: 30px;
+  padding: 10px;
+  cursor: pointer;
+  text-align: center;
+  `
 const ButtonWhite = styled(Button)`
-border: solid 1px #fff;
-background: #fff;
-color: ${Black};
-`
-const ButtonBlack = styled(Button)`
-border: solid 1px #fff;
-font-size: 30px;
-padding: 10px;
-color: #fff;
-&:hover {
-  color: ${Black};
+  border: solid 1px #fff;
   background: #fff;
-}
-${props => props.submit && css`
+  color: ${Black};
+  `
+const ButtonBlack = styled(Button)`
+  border: solid 1px #fff;
+  font-size: 30px;
+  padding: 10px;
+  color: #fff;
+  &:hover {
+    color: ${Black};
+    background: #fff;
+  }
+  ${props => props.submit && css`
 width: 100%;
 margin: 50px auto;
 `}
-`
+  `
 const Scene2 = styled.div`
-background: #3B323E;
-display: flex;
-justify-content: center;
-align-items: center;
-padding: 40px 0;
-`
+  background: #3B323E;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px 0;
+  `
 const SubTitle = styled.div`
-font-size: ${props => props.big ? '60px' : '40px'};
-display: flex;
-justify-content: center;
-margin: 10px 0 10px 0;
-`
+  font-size: ${props => props.big ? '60px' : '40px'};
+  display: flex;
+  justify-content: center;
+  margin: 10px 0 10px 0;
+  `
 const ListWrapper = styled.div`
-justify-content: center;
-margin: 0 auto;
-font-size: 25px;
-line-height: 1.5;
-`
+  justify-content: center;
+  margin: 0 auto;
+  font-size: 25px;
+  line-height: 1.5;
+  `
 const List = styled.div`
-font-size: ${props => props.eng ? '18px' : '25px'};
-line-height: 1.5;
-`
+  font-size: ${props => props.eng ? '18px' : '25px'};
+  line-height: 1.5;
+  `
 const Scene3 = styled.div`
-filter: grayscale(80%);
-background-image: url('/static/scene3.jpg');
-background-size:cover;
-width: 100%;
-display: flex;
-align-items: center;
-transition: all .3s;
-min-height: 600px;
-text-align: center;
-&:hover {
-  filter: grayscale(0%);
-}
-`
+  filter: grayscale(80%);
+  background-image: url('/static/scene3.jpg');
+  background-size:cover;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  transition: all .3s;
+  min-height: 600px;
+  text-align: center;
+  &:hover {
+    filter: grayscale(0%);
+  }
+  `
 const TopicBackground = styled.div`
-background: rgba(0,0,0,.7);
-margin: auto;
-padding: 45px;
-width: 80%;
-`
+  background: rgba(0,0,0,.7);
+  margin: auto;
+  padding: 45px;
+  width: 80%;
+  `
 const CyberNomadSpirit = styled(Scene2)`
-height: 500px;
-`
+  height: 500px;
+  `
 const Scene5 = styled.div`
-background: #171616;
-`
+  background: #171616;
+  `
 const DateScene = styled.div`
-background: #fff;
-color: #4A4A4A;
-`
+  background: #fff;
+  color: #4A4A4A;
+  `
 const DateSceneContentWrapper = styled.div`
-padding: 20px;
-color: #4A4A4A;
-text-align: left;
-font-size: 20px;
-line-height: 2;
-display: flex;
-align-items: center;
-justify-content: space-around;
-@media(max-width: 768px) {
-flex-direction: column;
-}
-`
+  padding: 20px;
+  color: #4A4A4A;
+  text-align: left;
+  font-size: 20px;
+  line-height: 2;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  @media(max-width: 768px) {
+    flex-direction: column;
+  }
+  `
 const PhotoContainer = styled.div`
-margin: auto;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-width: 100%;
-`
-const PhotoWrapper = styled.div`
-display: flex;
-justify-content: space-around;
-@media(max-width: 768px) {
+  margin: auto;
+  display: flex;
   flex-direction: column;
-}
-`
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  `
+const PhotoWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  @media(max-width: 768px) {
+    flex-direction: column;
+  }
+  `
 const Photo = styled.img`
-align-self: center;
-margin: 25px;
-filter: grayscale(80%);
-border-radius: 50%;
-width: ${props => props.big ? '250px' : '180px'};
-height: ${props => props.big ? '250px' : '180px'};
-transition: all .3s;
-filter: grayscale(80%);
-&:hover {
-  filter: grayscale(0%);
-}
-`
+  align-self: center;
+  margin: 25px;
+  filter: grayscale(80%);
+  border-radius: 50%;
+  width: ${props => props.big ? '250px' : '180px'};
+  height: ${props => props.big ? '250px' : '180px'};
+  transition: all .3s;
+  filter: grayscale(80%);
+  &:hover {
+    filter: grayscale(0%);
+  }
+  `
 const Lecturer = styled.div`
-display: flex;
-flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+  `
 const CoOrganizer = styled.div`
-display: flex;
-flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+  `
 const TitleOftheEvent = styled.div`
-display: flex;
-align-self: center;
-font-size: 20px;
-`
+  display: flex;
+  align-self: center;
+  font-size: 20px;
+  `
 const Name = styled.div`
-font-size: ${ props => props.small ? '20px' : '40px'};
-display: flex;
-align-self: center;
-line-height: 2;
-`
+  font-size: ${ props => props.small ? '20px' : '40px'};
+  display: flex;
+  align-self: center;
+  line-height: 2;
+  `
 const Scene6 = styled.div`
-background: #070808;
-`
+  background: #070808;
+  `
 const FormWrapper = styled.div`
-width: 60%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-margin: auto;
-padding-top: 10vh;
-& > input {
-  margin: 20px 20px 65px;
-}
-& > textarea {
-  margin: 20px;
-  resize: none;
-  height: 150px;
-}
-@media(max-width: 1024px){
-width: 100%;
-}
-`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  padding-top: 10vh;
+  & > input {
+    margin: 20px 20px 65px;
+  }
+  & > textarea {
+    margin: 20px;
+    resize: none;
+    height: 150px;
+  }
+  @media(max-width: 1024px){
+    width: 100%;
+  }
+  `
 const Introduction = styled.div`
-width: 100%;
-font-size: 15px;
-line-height: 1.5;
-text-align: center;
-`
+  width: 100%;
+  font-size: 15px;
+  line-height: 1.5;
+  text-align: center;
+  `
 const Footer = styled.div`
-width: 100%;
-height: 300px;
-font-size: 15px;
-display: flex;
-align-items: center;
-background: #111111;
-`
+  width: 100%;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  background: #111111;
+  `
 const Description = styled.div`
-font-size: 14px;
-line-height: 1.5;
-text-align: center;
-`
+  font-size: 14px;
+  line-height: 1.5;
+  text-align: center;
+  `
 const FormLoadingWrapper = styled.div`
-height: 415px;
-text-align: center;
-display: flex;
-justify-content: center;
-align-items: center;
-`
+  height: 415px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  `
 const ListWrapperInner = styled.div`
-margin-top: 20px;
-font-size: 18px;
-`
+  margin-top: 20px;
+  font-size: 18px;
+  `
 const WhatYouWillLearn = styled.div`
-font-size: 20px;
-line-height: 2;
-text-align: left;
-margin: auto;
-width: 80%;
-`
+  font-size: 20px;
+  line-height: 2;
+  text-align: left;
+  margin: auto;
+  width: 80%;
+  `
 const Map2 = styled.img`
-width: 40vw;
-@media(max-width: 768px) {
-width: 100%;
-}
-`
+  width: 40vw;
+  @media(max-width: 768px) {
+    width: 100%;
+  }
+  `
 const SuccessSentWrapper = styled.div`
-height: 415px;
-text-align: center;
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-@media(max-width: 768px) {
-  height: auto;
-}
-`
+  height: 415px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  @media(max-width: 768px) {
+    height: auto;
+  }
+  `
 const SuccessSentInnerWrapper = styled.div`
-font-size: 25px;
-line-height: 2;
-width: 50%;
-`
+  font-size: 25px;
+  line-height: 2;
+  width: 50%;
+  `
 const FaceBookIcon = styled.div`
-display: inline-block;
-width: 50px;
-height: 50px;
-background: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4IgogICAgIHdpZHRoPSI1MCIgaGVpZ2h0PSI1MCIKICAgICB2aWV3Qm94PSIwIDAgNTAgNTAiCiAgICAgc3R5bGU9IjtmaWxsOiNjY2NjY2M7IgogICAgIGNsYXNzPSJpY29uIGljb25zOC1mYWNlYm9vay1maWxsZWQiPiAgICA8cGF0aCBkPSJNNDEsNEg5QzYuMjQsNCw0LDYuMjQsNCw5djMyYzAsMi43NiwyLjI0LDUsNSw1aDMyYzIuNzYsMCw1LTIuMjQsNS01VjlDNDYsNi4yNCw0My43Niw0LDQxLDR6IE0zNywxOWgtMmMtMi4xNCwwLTMsMC41LTMsMiB2M2g1bC0xLDVoLTR2MTVoLTVWMjloLTR2LTVoNHYtM2MwLTQsMi03LDYtN2MyLjksMCw0LDEsNCwxVjE5eiI+PC9wYXRoPjwvc3ZnPg==') 50% 50% no-repeat;
-background-size: 100%;
-`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  background: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4IgogICAgIHdpZHRoPSI1MCIgaGVpZ2h0PSI1MCIKICAgICB2aWV3Qm94PSIwIDAgNTAgNTAiCiAgICAgc3R5bGU9IjtmaWxsOiNjY2NjY2M7IgogICAgIGNsYXNzPSJpY29uIGljb25zOC1mYWNlYm9vay1maWxsZWQiPiAgICA8cGF0aCBkPSJNNDEsNEg5QzYuMjQsNCw0LDYuMjQsNCw5djMyYzAsMi43NiwyLjI0LDUsNSw1aDMyYzIuNzYsMCw1LTIuMjQsNS01VjlDNDYsNi4yNCw0My43Niw0LDQxLDR6IE0zNywxOWgtMmMtMi4xNCwwLTMsMC41LTMsMiB2M2g1bC0xLDVoLTR2MTVoLTVWMjloLTR2LTVoNHYtM2MwLTQsMi03LDYtN2MyLjksMCw0LDEsNCwxVjE5eiI+PC9wYXRoPjwvc3ZnPg==') 50% 50% no-repeat;
+  background-size: 100%;
+  `
